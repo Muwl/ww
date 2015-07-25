54409044e92f06137b535d8cf0be3c5db01a3c0c
Muwl <itexplorer@126.com>
2015-07-25 13:48:43 +0800

    awdwad

---
+++ src/aaa/ddd/AD.java
@@ -0,0 +1,12 @@
+package aaa.ddd;
+
+public class AD {
+	public static void main(String[] args) {
+		for (int i = 10; i >=0; i--) {
+			System.out.println("aaa"+i);
+			System.err.println("wakjdykayd");
+			
+		}
+		
+	}
+}
